0b8d1110db3309f393f3da5069495713a738de6a
iiom <ioma766@gmail.com>
2019-03-26 13:07:31 +0300

    asd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,13 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    render text: @items.map { |i| "#{i.name}: #{i.price}" }.join("<br/>")
   end
 
   # /items/1 GET
   def show
+    unless @item = Item.where(id: params[:id]).first
+      render text: "Page not found", status: 404
+    end
   end
 
   # /items/new GET
